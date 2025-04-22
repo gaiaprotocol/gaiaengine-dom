a1033e6dff80756a55f5532cfc43d22bd98513d4
yjgaia <yjdev88@gmail.com>
2025-04-22 14:35:35 +0900

    Add SpritesheetData export to index files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as AnimatedSprite } from "./image/AnimatedSprite.js";
 export { default as Sprite } from "./image/Sprite.js";
+export { default as SpritesheetData } from "./image/SpritesheetData.js";
 export { default as Fullscreen } from "./screen/Fullscreen.js";
 export { default as GameScreen } from "./screen/GameScreen.js";
 
